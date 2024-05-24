18aa45486fcbdf3cf9734fcb7c96b56f13f1a2da
Matthieu MIGNE <matthieu.migne@orange.fr>
2024-05-24 16:31:51 +0200

    swagger fix

--- src/util/swagger.js
+++ src/util/swagger.js
@@ -4,6 +4,7 @@ const outputFile = 'swagger_output.json'
 const endpointsFiles = ['src/routes/router.js']
 
 swaggerAutogen(outputFile, endpointsFiles, {
+  host: 'stock-blog-backend.osc-fr1.scalingo.io',
   securityDefinitions: {
     apiKeyAuth: {
       type: 'apiKey',
